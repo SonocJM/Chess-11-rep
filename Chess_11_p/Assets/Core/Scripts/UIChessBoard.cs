57392fc526af1355ea0fcc928590a5a4e760b11d
MasterPedro1 <pedromoralescaba@gmail.com>
2024-11-25 13:14:34 -0600

    hdri y otras cosas que ya ni me acuerdo

--- Chess_11_p/Assets/Core/Scripts/UIChessBoard.cs
+++ Chess_11_p/Assets/Core/Scripts/UIChessBoard.cs
@@ -7,11 +7,26 @@ using System;
 
 public class UIChessBoard : MonoBehaviour
 {
+    [Header("p1")]
     public TextMeshProUGUI cooldownP1;
+    public GameObject buttonP1;
+    public GameObject portraitP1;
+    [Header("p2")]
     public TextMeshProUGUI cooldownP2;
+    public GameObject buttonP2;
+    public GameObject portraitP2;
+    [Header("general")]
     public GenerateBoard cB;
+    public int p1Team;
+    public int p2Team;
+    public Image[] AbilityIcons;
+    public Image[] CharacterIcons;
     
+    
+    public void UniversalIndex()
+    {
 
+    }
     public void UpdateUI()
     {
         UpdateCooldowns();
@@ -19,6 +34,8 @@ public class UIChessBoard : MonoBehaviour
     private void Start()
     {
         UpdateCooldowns();
+        p1Team = cB.p1T;
+        p2Team = cB.p2T;
     }
 
     private void UpdateCooldowns()
